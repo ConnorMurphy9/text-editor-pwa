635b5b1e24f8511f789f70aaffc5bc2f131e4083
William <110736589+ConnorMurphy9@users.noreply.github.com>
2022-11-14 20:05:20 -0500

    finished app

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,28 +13,39 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-// export const putDb = async (content) => {
+export const putDb = async (content) => {
   
-//   // Create a connection to the database and version we want to use.
-//   const contactDb = await openDB('contact', 1);
+  // Create a connection to the database and version we want to use.
+  const contactDb = await openDB('jate', 1);
 
-//   // Create a new transaction and specify the database and data privileges.
-//   const tx = contactDb.transaction('contact', 'readwrite');
+  // Create a new transaction and specify the database and data privileges.
+  const tx = contactDb.transaction('jate', 'readwrite');
 
-//   // Open up the desired object store.
-//   const store = tx.objectStore('contact');
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
 
-//   // Use the .add() method on the store and pass in the content.
-//   const request = store.add({ name: name, home_phone: home, cell_phone: cell, email: email });
+  // Use the .add() method on the store and pass in the content.
+  const request = store.put({ id: 1, value: content });
 
-//   // Get confirmation of the request.
-//   const result = await request;
-//   console.log('🚀 - data saved to the database', result);
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
   
-//   console.error('putDb not implemented');
-// }
+
+}
 
 // // TODO: Add logic for a method that gets all the content from the database
-// export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const contactDb = await openDB('jate', 1);
+
+  const tx = contactDb.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.get(1);
+  const result = await request;
+
+  result ? console.log('🚀 - data retrieved from the database', result.value) :  console.log('🚀 - no data in the database');
+}
 
 initdb();
